ac12ef8a4352b3cfb72a4c732e4c8c6b356e3381
Joseph Satterfield <jjs228@case.edu>
2017-02-04 21:20:07 -0500

    Some basic placeholder. Let's see how bad this goes.
    Signed-off-by: Joseph Satterfield <jjs228@case.edu>

---
+++ robo-chihuahua.js
@@ -0,0 +1,10 @@
+"use strict";
+var request = require('request')
+
+var RoboChihuahua = function(config) {
+    if(!(this instanceof RoboChihuahua)) {
+        return new RoboChihuahua(config)
+    }
+    this.username = config.username;
+    this.password = config.password;
+}
